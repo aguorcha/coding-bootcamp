afd5d5330a6a8b48a0e8d0fc533f95b3e34763c9
Agu Orcha <aguorcha@gmail.com>
2024-09-22 18:51:00 +0200

    Lógica implementada. Proyecto terminado

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,32 @@
 <script setup>
+import { ref, computed } from 'vue';
 
+const testimonials = ref([
+{
+    image: 'https://i.postimg.cc/qv4MqRTp/image-tanya.jpg',
+    testimonial: "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+    name: 'Tanya Sinclair',
+    role: 'UX Engineer',
+  },
+  {
+    image: 'https://i.postimg.cc/503mcjbj/image-john.jpg',
+    testimonial: "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    name: 'John Tarkpor',
+    role: 'Junior Front-end Developer',
+  }
+]);
+
+const currentIndex = ref(0);
+
+const goToPrev = () => {
+  currentIndex.value = (currentIndex.value - 1 + testimonials.value.length) % testimonials.value.length;
+};
+
+const gotToNext = () => {
+  currentIndex.value = (currentIndex.value + 1) % testimonials.value.length;
+}
+
+const currentTestimonial = computed(() => testimonials.value[currentIndex.value]);
 </script>
 
 <template>
@@ -13,17 +40,17 @@
       >
 
       <img 
-        src="../assets/images/image-tanya.jpg" 
-        alt="Tanya"
+        :src="currentTestimonial.image" 
+        alt="Current testimonial"
         class="relative z-10 w-[64%] sm:w-[62%] rounded object-cover mx-auto mt-6 sm:mt-10 shadow-photo lg:w-[64%]"
       >
 
       <div class="relative w-20 left-1/2 transform -translate-x-1/2 lg:left-52 lg:translate-x-0 bottom-5 z-20 flex">
-        <button class="bg-white w-full rounded-l-full p-3">
+        <button @click="goToPrev" class="bg-white w-full rounded-l-full p-3">
           <img src="../assets/images/icon-prev.svg" alt="Left arrow" class="w-3 h-3">
         </button>
 
-        <button class="bg-white w-full rounded-r-full p-3">
+        <button @click="gotToNext" class="bg-white w-full rounded-r-full p-3">
           <img src="../assets/images/icon-next.svg" alt="Right arrow" class="w-3 h-3">
         </button>
       </div>
@@ -35,10 +62,10 @@
         alt="Background image"
         class="relative w-14 lg:w-20 lg:left-44 lg:-top-16"
       >
-      <p class="w-full -mt-6 font-light lg:absolute lg:left-32 lg:text-xl">“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”</p>
+      <p class="w-full -mt-6 font-light lg:absolute lg:left-32 lg:text-xl">{{ currentTestimonial.testimonial }}</p>
       <div class="mt-4 lg:flex lg:ml-32 lg:mt-10">
-        <h2 class="font-bold">Tanya Sinclair</h2>
-        <span class="text-grayish-blue">UX Engineer</span>
+        <h2 class="font-bold">{{ currentTestimonial.name }}</h2>
+        <span class="text-grayish-blue lg:ml-2">{{ currentTestimonial.role }}</span>
       </div>
     </div>
 
